0e66157f75754ce42dd3e527a3ac72399277fefc
peu-94 <70969444+peu-94@users.noreply.github.com>
2025-07-30 02:24:40 +0530

    home page creation

---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="min-h-screen">
+      <div>
+        <h2>MatchMingle</h2>
+        <img
+          src="https://images.unsplash.com/photo-1475721042966-f829c9b42aab?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWF0cmltb255fGVufDB8fDB8fHww"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
